b6ab24ec8268f367d1f64e37439b82f8d8fbfc11
erka <erik.westerberg@hotmail.se>
2019-07-23 13:33:22 +0200

    created a ball class, made speed function and did update function on ball ball is now moving

---
+++ src/ball.js
@@ -0,0 +1,21 @@
+export default class Ball {
+
+    constructor() {
+
+        this.image = document.getElementsByClassName("img_ball");
+        this.position = {x: 10, y: 10}
+        this.speed = {x: 2, y: 2};
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, this.position.x, this.position.y, 16, 16);
+
+    }
+
+
+    update(deltaTime) {
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Paddle from '/src/paddle.js';
-import InputHandler from "/src/input";
+import InputHandler from "/src/input.js";
+import Ball from "/src/ball.js"
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -8,18 +9,25 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-
+let ball = new Ball();
 new InputHandler(paddle);
 
 let lastTime = 0;
+
+// images
+
+let imgBall = document.getElementById("img_ball");
+
 function gameLoop(timestamp) {
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
 
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
-  game.update(deltaTime);
-  game.draw(ctx);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+  ball.update(deltaTime) 
+  ball.draw(ctx)
 
   requestAnimationFrame(gameLoop);
 }

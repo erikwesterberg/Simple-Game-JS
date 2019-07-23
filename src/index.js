f2f17bcb83f8bb17f0079719c36df9e099df0660
erka <erik.westerberg@hotmail.se>
2019-07-23 13:16:59 +0200

    created movement to the ground brick, also imported a ball to screen

--- src/index.js
+++ src/index.js
@@ -1,45 +1,31 @@
-import Paddle from "/src/paddle";
+import Paddle from '/src/paddle.js';
 import InputHandler from "/src/input";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-// const sizing, 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
-//Clear screen
-ctx.clearRect(0, 0, 800, 600);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
-paddle.draw(ctx);
-
-new InputHandler(paddle)
+new InputHandler(paddle);
 
 let lastTime = 0;
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
 
-const gameLoop = (timestamp) => {
-    let deltaTime = timestamp - lastTime
-    ctx.clearRect(0, 0, 800, 600);
-    paddle.update(deltaTime);
-    paddle.draw(ctx);
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
-    requestAnimationFrame(gameLoop)
+  game.update(deltaTime);
+  game.draw(ctx);
 
+  requestAnimationFrame(gameLoop);
 }
 
-
-gameLoop();
-
-// //give squeare colour
-// ctx.fillStyle = "#f00";
-
-// //draw a squeare on page
-// ctx.fillRect(20, 20, 100, 100);
+requestAnimationFrame(gameLoop);
 
 
-// //give squeare colour
-// ctx.fillStyle = "#00f";
 
-// //draw a squeare on page
-// ctx.fillRect(240, 200, 50, 50);
+
--- src/paddle.js
+++ src/paddle.js
@@ -23,6 +23,10 @@ export default class Paddle {
          this.speed = this.maxSpeed
      }
 
+     stop() {
+        this.speed = 0
+     }
+
     draw(ctx) {
     ctx.fillStyle = "#0ff"
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
